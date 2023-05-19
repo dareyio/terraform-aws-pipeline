76c4866687e858f7ff3549e5bccd5f4614040e08
dare <dare.olufunmilayo@gmail.com>
2023-05-19 15:09:25 +0100

    deploy stage

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_availability_zones" "available" {}
 locals {
   name            = "ex-${replace(basename(path.cwd), "_", "-")}"
   cluster_version = "1.24"
-  region          = "eu-west-1"
+  region          = "eu-west-2"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
