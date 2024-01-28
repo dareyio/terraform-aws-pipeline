40a587ea7d3a2d2f23f583829f237369d7bae255
Busi <pietersonbusi@gmail.com>
2024-01-28 14:03:14 +0200

    new commit

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "busi-cicd-class"
+    bucket = "busipieterson-cicd-pipeline"
     key    = "terraform.tfstate"
     region = "eu-west-2"
   }
