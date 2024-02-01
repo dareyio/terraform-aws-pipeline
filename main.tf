1f84abee5304388b12765d23abcbb2bf86ac6de0
Busi <pietersonbusi@gmail.com>
2024-02-01 20:36:53 +0200

    added new resource

--- main.tf
+++ main.tf
@@ -452,3 +452,10 @@ data "aws_ami" "eks_default_bottlerocket" {
     values = ["bottlerocket-aws-k8s-${local.cluster_version}-x86_64-*"]
   }
 }
+
+resource "aws_eks_addon" "example" {
+  cluster_name                = aws_eks_cluster.example.name
+  addon_name                  = "coredns"
+  addon_version               = "v1.10.1-eksbuild.1" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
+  resolve_conflicts_on_update = "PRESERVE"
+}
