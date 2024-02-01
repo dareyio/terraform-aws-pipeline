b9752071356bfed48869d3e5707db0965f4f52d1
Busi <pietersonbusi@gmail.com>
2024-02-01 20:40:25 +0200

    update

--- main.tf
+++ main.tf
@@ -453,9 +453,4 @@ data "aws_ami" "eks_default_bottlerocket" {
   }
 }
 
-resource "aws_eks_addon" "example" {
-  cluster_name                = aws_eks_cluster.example.name
-  addon_name                  = "coredns"
-  addon_version               = "v1.10.1-eksbuild.1" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
-  resolve_conflicts_on_update = "PRESERVE"
-}
+
